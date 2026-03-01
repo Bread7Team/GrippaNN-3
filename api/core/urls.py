63a6b2fdddf778e74c2b25f52dbcffbedc96ceb1
Ivan Ionov <team@grippann.ru>
2026-03-01 09:16:54 +0300

    feat: Очистка проекта до скелета: удаление операций и истории

--- api/core/urls.py
+++ api/core/urls.py
@@ -2,13 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
-    OrganizationViewSet,
-    DepartmentViewSet,
-    PositionViewSet,
-    EmployeeViewSet,
-    FileViewSet,
-    HrOperationViewSet,
-    ChangeHistoryViewSet,
     UserViewSet,
     LoginView,
     RegisterView,
@@ -16,13 +9,6 @@ from .views import (
 )
 
 router = DefaultRouter()
-router.register(r'organizations', OrganizationViewSet, basename='organization')
-router.register(r'departments', DepartmentViewSet, basename='department')
-router.register(r'positions', PositionViewSet, basename='position')
-router.register(r'employees', EmployeeViewSet, basename='employee')
-router.register(r'files', FileViewSet, basename='file')
-router.register(r'hr-operations', HrOperationViewSet, basename='hr-operation')
-router.register(r'change-history', ChangeHistoryViewSet, basename='change-history')
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
--- api/core/views/__init__.py
+++ api/core/views/__init__.py
@@ -1,11 +1,7 @@
-from .hr_operation import HrOperationViewSet
-from .change_history import ChangeHistoryViewSet
 from .user import UserViewSet
 from .auth import LoginView, RegisterView, MeView
 
 __all__ = [
-    'HrOperationViewSet',
-    'ChangeHistoryViewSet',
     'UserViewSet',
     'LoginView',
     'RegisterView',
