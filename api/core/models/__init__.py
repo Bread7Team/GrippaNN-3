ad3818bca11fd4ed5f1e211507e58dcabaf05d28
Masha <team@grippann.ru>
2026-03-01 09:14:10 +0300

    feat: Удаление модулей справочников (организации, отделы, должности)

--- api/core/models/__init__.py
+++ api/core/models/__init__.py
@@ -1,15 +1,9 @@
 from .user import User
-from .organization import Organization
-from .department import Department
-from .position import Position
 from .hr_operation import HrOperation
 from .change_history import ChangeHistory
 
 __all__ = [
     'User',
-    'Organization',
-    'Department',
-    'Position',
     'HrOperation',
     'ChangeHistory',
 ]
